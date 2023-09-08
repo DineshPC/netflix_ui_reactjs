79967d9d8122c921533dc1995330c3dd85887bd0
Dinesh <dineshcoudhary38@gmail.com>
2023-09-09 00:33:29 +0530

    fix bug

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
                 </div>
                 <div className='navBarBtn'>
-                    <button>Join Now</button>
+                    <button style={{'backgroundColor':'red', 'color':'white'}}>Join Now</button>
                     <button>Sign Up</button>
                 </div>
             </nav>
