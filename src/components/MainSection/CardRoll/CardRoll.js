89af34d8c5285935f388097e670227565be2007a
Dinesh <dineshcoudhary38@gmail.com>
2023-09-09 21:04:21 +0530

    second commit

--- src/components/MainSection/CardRoll/CardRoll.js
+++ src/components/MainSection/CardRoll/CardRoll.js
@@ -4,12 +4,33 @@ import img1 from "../../../assert/lucifer.jpg"
 import "./CardRollStyle.css"
 
 export default function CardRoll(props) {
+
+  let cardId = props.cardId;
+  let a = "#"+cardId
+  let box = document.querySelector(a.toString());
+
+  let click_previous_btn =  () => {
+    let width =  box.clientWidth
+    box.scrollLeft = box.scrollLeft - (width/1.1)
+  }
+
+  let click_next_btn = () => {
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft + (width/1.1)
+  }
+
   return (
     <div className='cardRollContainer'>
         <div className='cardHeading'>
             <p className=''>{props.name}</p>
         </div>
-        <div className='cardRollList'>
+        <div className='back-forward-btn'>
+          <button className='previous-btn' onClick={click_previous_btn}>&lt;</button>
+          <button className='next-btn' onClick={click_next_btn}>&gt;</button>
+        </div>
+        <div className='cardRollList' id={props.cardId}>
+            <Card name="movie1" img={img1}/>
+            <Card name="movie1" img={img1}/>
             <Card name="movie1" img={img1}/>
             <Card name="movie1" img={img1}/>
             <Card name="movie1" img={img1}/>
@@ -19,6 +40,7 @@ export default function CardRoll(props) {
             <Card name="movie1" img={img1}/>
             <Card name="movie1" img={img1}/>
         </div>
+        
     </div>
     
   )
--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -7,9 +7,9 @@ export default function MainSection() {
   return (
     <div className='MainContainer'>
       <Intro />
-      <CardRoll name="Popular on Netflix"/>
-      <CardRoll name="TV Comedies"/>
-      <CardRoll name="Bingeworthy TV Dramas"/>
+      <CardRoll name="Popular on Netflix" cardId="cardID1"/>
+      <CardRoll name="TV Comedies" cardId="cardID2"/>
+      <CardRoll name="Bingeworthy TV Dramas" cardId="cardID3"/>
     </div>
   )
 }
